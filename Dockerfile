ff21ec861586a4e75b6b6ba6843dcd81cbffe839
tatd3v <tatdev91@gmail.com>
2025-01-21 05:46:22 -0500

    last changes

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.12.7-slim
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install --no-cache-dir --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 80
+
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- run.py
+++ run.py
@@ -5,4 +5,4 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "app")))
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=80, reload=True)
