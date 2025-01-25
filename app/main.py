5a36666d902550a9c58306f1d001e125ac93a09b
tatd3v <tatdev91@gmail.com>
2025-01-25 18:03:05 -0500

    change prefix for youtube route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import router  # Correct import for router
+from app.routes.youtube import youtube_router
+
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+YOUTUBE_CHANNEL_ID = os.getenv("YOUTUBE_CHANNEL_ID")
+
 
 app = FastAPI(title="Ballroom Medellin API")
 
@@ -12,4 +17,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(router, prefix="/api/v1")
+app.include_router(youtube_router, prefix="/api/v1/youtube")
+
+
+@app.get("/youtube-info")
+async def youtube_info():
+    return {
+        "api_key": YOUTUBE_API_KEY,
+        "channel_id": YOUTUBE_CHANNEL_ID,
+    }
