1be65c61cb1d778228702d9895adf7a0f0a0141e
tatd3v <tatdev91@gmail.com>
2025-01-25 20:38:26 -0500

    add cors origin

--- app/main.py
+++ app/main.py
@@ -9,13 +9,25 @@ YOUTUBE_CHANNEL_ID = os.getenv("YOUTUBE_CHANNEL_ID")
 
 app = FastAPI(title="Ballroom Medellin API")
 
+origins = [
+    "http://localhost:5173",
+    "https://ballroomedellin.com",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.middleware("http")
+async def add_cors_headers(request: Request, call_next):
+    response = await call_next(request)
+    print("Response Headers:", response.headers)
+    return response
+
 
 app.include_router(youtube_router, prefix="/api/v1/youtube")
 
