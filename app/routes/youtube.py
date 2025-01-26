58737fcc245377d0340ba7410738df49ca989bb0
tatd3v <tatdev91@gmail.com>
2025-01-26 02:09:39 -0500

    refine code

--- app/routes/youtube.py
+++ app/routes/youtube.py
@@ -6,15 +6,13 @@ from models import Playlist
 
 youtube_router = APIRouter()
 
-
 @youtube_router.get("/", response_model=List[Playlist])
 async def get_playlists_with_videos():
     youtube_service = YoutubeService()
 
     try:
-        playlists = youtube_service.fetch_playlists_with_videos()
-        if not playlists:
-            raise HTTPException(status_code=404, detail="Playlists not found")
-        return playlists
+        if playlists := youtube_service.fetch_playlists_with_videos():
+            return playlists
+        raise HTTPException(status_code=404, detail="Playlists not found")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e)) from e
