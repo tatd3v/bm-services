773fa283505d880bad32d5bf611a19086da8697e
tatd3v <tatdev91@gmail.com>
2025-05-31 10:45:23 -0500

    add class settings

--- app/utils/config.py
+++ app/utils/config.py
@@ -4,7 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Youtube Service Config Static Variables
+
+class Settings:
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+
+
+settings = Settings()
+
 
 YOUTUBE_API_BASE_URL = "https://www.googleapis.com/youtube/v3"
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
